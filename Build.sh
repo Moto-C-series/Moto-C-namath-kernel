192098ad8de48097d699e3adfa52bb34daebc304
DhirajSurvase <dms25121997@gmail.com>
2018-03-08 07:06:22 +0000

    unbrick for gcc-7.x

--- Build.sh
+++ Build.sh
@@ -5,6 +5,18 @@
 
 mkdir -p out
 
+#host and user
+export KBUILD_BUILD_USER="DhirajMS"
+export KBUILD_BUILD_HOST="Dracarys"
+
+#get toolchain gcc-4.8
+#git clone https://github.com/DhirajSurvase/GCC_4.8 gcc4.8
+#export CROSS_COMPILE=/home/younilban/kernel/panelli/gcc4.8/bin/arm-eabi-
+
+#tc>arm-linaro-7.x
+git clone https://github.com/nathanchance/gcc-prebuilts -b arm-linaro-7.x arm-linaro-7.x
+export CROSS_COMPILE=/home/younilban/moto/namath/arm-linaro-7.x/bin/arm-linaro-linux-androideabi-
+
 export USE_CCACHE=1
 
 export ARCH=arm ARCH_MTK_PLATFORM=mt6735
